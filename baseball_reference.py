b985ab21d35c93d6d7f983c3119fe8a0115711ec
Lacy Morrow <me@lacymorrow.com>
2014-11-09 15:06:10 -0800

    Made things better

--- baseball_reference.py
+++ baseball_reference.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python
 
-import urllib, csv, re, itertools, sys
+import urllib, csv, re, itertools, sys, time
 from BeautifulSoup import BeautifulSoup
 from string import ascii_letters
 
 YEAR = 2008
+BIRTHDATE_VS_AGE = False
 
 PLAYERS_PAGE_TEMPLATE='http://www.baseball-reference.com/players/%(letter)s/'
 
@@ -180,7 +181,7 @@ def get_batting_stats(soup):
         stats = decompose_batting_table(batting_table)
         return stats
 
-def get_long_player_name(soup):
+def get_long_player_name(soup, year):
     '''Gets a more specific name from the player page to avoid duplicate names.
 
     '''
@@ -194,115 +195,139 @@ def get_long_player_name(soup):
 
     return long_name_element.text
 
-def get_short_player_name(soup):
+def get_short_player_name(soup, year):
     short_name_element = soup.find(id='player_name')
     return short_name_element.text
 
-def get_age(soup):
-    return ''
+def get_age_hometown(soup, year):
+    age_hometown = ''
+    age_hometown = soup.find(id='necro-birth')
+    age_hometown = re.search('Born:\s*(\D+\d+[\, ]*\d+)\s*in\s*([\w.,!@#$%^&*a-zA-Z ]*)', age_hometown.text, re.IGNORECASE)
 
-def get_current_team(soup):
-    teams = soup.find(id='batting_standard').findAll('tr')[1:]
+    if BIRTHDATE_VS_AGE:
+        return (age_hometown.group(1), age_hometown.group(2))
+    else:
+        return (year - time.strptime(age_hometown.group(1), '%B %d,%Y')[0], age_hometown.group(2))
+
+
+def get_current_team(soup, year):
+    elem = 'batting_standard.' + str(year)
+    teams = soup.findAll(id=elem)
     current_team = ''
     for row in teams:
         r = row.findAll('td')
-        print r[2].text
         if len(r) > 2:
             if current_team == '':
                 current_team =  r[2].text
             else:
                 current_team += ', ' + r[2].text
-    current_team = None
     return current_team
 
-def get_current_salary(soup):
+def get_current_salary(soup, year):
     try:
         salaries = soup.find(id='salaries').findAll('tr')[1:]
         player_salary = ''
         for row in salaries:
             r = row.findAll('td')
-            if r[0].text == str(YEAR) and len(r) > 3 and r[0].text.isdigit():
+            if r[0].text == str(year) and len(r) > 3 and r[0].text.isdigit():
                 player_salary =  r[3].text
     except:
         player_salary = None
     return player_salary
 
-def get_prior_team(soup):
-    return ''
+def get_prior_team(soup, year):
+    prior_team = ''
+    for i in range(year-25,year):
+        elem = 'batting_standard.' + str(i)
+        teams = soup.findAll(id=elem)
+        for row in teams:
+            r = row.findAll('td')
+            if prior_team == '':
+                prior_team =  r[2].text
+            else:
+                prior_team += ', ' + r[2].text
+    # prior_team = None
+    return prior_team
 
-def get_position(soup):
-    return ''
+def get_position(soup, year):
+    position = ''
+    position = soup.find(attrs={"itemprop" : "role"})
+    return position.contents[0]
 
-def get_hometown(soup):
-    return ''
+def get_nationality(soup, year):
+    flag = ''
+    flag = soup.find("span", {"class":re.compile(r"\bflag\b")})
+    flag = re.search('(\w+)\s*$', flag['class'], re.IGNORECASE)
+    return flag.group(0)
 
-def get_nationality(soup):
-    return ''
-
-def get_war(soup):
+def get_war(soup, year):
     try:
-        war = soup.find(id='batting_value').findAll('tfoot')[0].findAll('td')[14].text
+        war = soup.find(id='batting_value').findAll('tfoot')[0].findAll('td')[13].text
     except:
         war = None
         pass
     return war
 
-def get_total_seasons(soup):
-    return ''
+def get_total_seasons(soup, year):
+    return '*'
 
-def get_is_multiple_teams(soup):
-    return ''
+def get_is_multiple_teams(soup, year):
+    return '*'
 
-def get_prior_year_current_team_wins(soup):
-    return ''
+def get_prior_year_current_team_wins(soup, year):
+    return '*'
 
-def get_prior_year_wins(soup):
-    return ''
+def get_prior_year_wins(soup, year):
+    return '*'
 
-def get_is_all_star(soup):
-    return ''
+def get_is_all_star(soup, year):
+    return '*'
 
 def get_all_player_stats():
     count = 0
     for player_name, player_page_url in get_all_player_page_links():
-        if count < 100:
-            count += 1
-            soup = url_to_beautiful_soup(player_page_url)
-            batting_stats = get_batting_stats(soup)
-            player_salary = get_current_salary(soup)
-            long_player_name = get_long_player_name(soup)
-            short_player_name = get_short_player_name(soup)
-            age = get_age(soup)
-            current_team = get_current_team(soup)
-            position = get_position(soup)
-            hometown = get_hometown(soup)
-            nationality = get_nationality(soup)
-            war = get_war(soup)
-            total_seasons = get_total_seasons(soup)
-            is_multiple_teams = get_is_multiple_teams(soup)
-            prior_team = get_prior_team(soup)
-            prior_year_current_team_wins = get_prior_year_current_team_wins(soup)
-            prior_year_wins = get_prior_year_wins(soup)
-            is_all_star = get_is_all_star(soup)
-        else:
-            yield {}
-
-        yield {
-            'name': short_player_name,
-            'age': age,
-            'current_team': current_team,
-            'position': position,
-            'hometown': hometown,
-            'nationality': nationality,
-            'salary': player_salary,
-            'war': war,
-            'total_seasons': total_seasons,
-            'total_seasons_none_if_multiple_teams': '' if is_multiple_teams else total_seasons,
-            'prior_team': prior_team,
-            'prior_year_current_team_wins': prior_year_current_team_wins,
-            'prior_year_wins': prior_year_wins,
-            'is_all_star': is_all_star
-        }
+        for year in range(YEAR-1,YEAR+1):
+            if count < 100:
+                count += 1
+                soup = url_to_beautiful_soup(player_page_url)
+                current_team = get_current_team(soup, year)
+                if current_team:
+                    batting_stats = get_batting_stats(soup)
+                    player_salary = get_current_salary(soup, year)
+                    long_player_name = get_long_player_name(soup, year)
+                    short_player_name = get_short_player_name(soup, year)
+                    age_hometown = get_age_hometown(soup, year)
+                    position = get_position(soup, year)
+                    nationality = get_nationality(soup, year)
+                    war = get_war(soup, year)
+                    total_seasons = get_total_seasons(soup, year)
+                    is_multiple_teams = get_is_multiple_teams(soup, year)
+                    prior_team = get_prior_team(soup, year)
+                    prior_year_current_team_wins = get_prior_year_current_team_wins(soup, year)
+                    prior_year_wins = get_prior_year_wins(soup, year)
+                    is_all_star = get_is_all_star(soup, year)
+            else:
+                yield {}
+            if current_team:
+                yield {
+                    'year': year,
+                    'name': short_player_name,
+                    'age': age_hometown[0],
+                    'current_team': current_team,
+                    'position': position,
+                    'hometown': age_hometown[1],
+                    'nationality': nationality,
+                    'salary': player_salary,
+                    'war': war,
+                    'total_seasons': total_seasons,
+                    'total_seasons_none_if_multiple_teams': '' if is_multiple_teams else total_seasons,
+                    'prior_team': prior_team,
+                    'prior_year_current_team_wins': prior_year_current_team_wins,
+                    'prior_year_wins': prior_year_wins,
+                    'is_all_star': is_all_star
+                }
+            else:
+                yield {}
 
 def main():
     csv_list = [
@@ -330,31 +355,32 @@ def main():
         ]
     ]
     for s in get_all_player_stats():
-        csv_list.append([
-            s['name'],
-            '', # Top 20
-            s['current_team'],
-            s['position'],
-            '', # Position Code
-            s['age'],
-            s['hometown'],
-            s['nationality'],
-            '', # Country Number
-            '', # Race
-            s['salary'],
-            s['war'],
-            s['total_seasons'],
-            s['total_seasons_none_if_multiple_teams'],
-            s['prior_team'],
-            s['prior_year_current_team_wins'],
-            s['prior_year_wins'],
-            s['is_all_star'],
-            '', # Trips to DL
-            ''  # Days on DL
-        ])
-        with open('test.csv', 'w') as fp:
-            a = csv.writer(fp, delimiter=',')
-            a.writerows(csv_list)
+        if(s and s['year'] == YEAR):
+            csv_list.append([
+                s['name'],
+                '', # Top 20
+                s['current_team'],
+                s['position'],
+                '', # Position Code
+                s['age'],
+                s['hometown'],
+                s['nationality'],
+                '', # Country Number
+                '', # Race
+                s['salary'],
+                s['war'],
+                s['total_seasons'],
+                s['total_seasons_none_if_multiple_teams'],
+                s['prior_team'],
+                s['prior_year_current_team_wins'],
+                s['prior_year_wins'],
+                s['is_all_star'],
+                '', # Trips to DL
+                ''  # Days on DL
+            ])
+            with open('test.csv', 'w') as fp:
+                a = csv.writer(fp, delimiter=',')
+                a.writerows(csv_list)
 
     with open('test.csv', 'w') as fp:
         a = csv.writer(fp, delimiter=',')
